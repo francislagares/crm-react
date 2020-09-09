1a3eb21fa546f06d2a9363eec5f4bdeb0124f41e
Francis Lagares <francis.lagares@gmail.com>
2020-09-09 20:10:58 +0200

    feat: create user model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
