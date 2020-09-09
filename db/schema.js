49e643737e96e6eb26d0161fea17025885a80f5a
Francis Lagares <francis.lagares@gmail.com>
2020-09-09 20:16:31 +0200

    feat: create gql user type

--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,27 @@ const { gql } = require('apollo-server');
 
 // Schema
 const typeDefs = gql`
+  input UserInput {
+    name: String!
+    lastName: String!
+    email: String!
+    password: String!
+  }
+
+  type User {
+    id: ID
+    name: String
+    lastName: String
+    email: String
+    createdAt: String
+  }
+
   type Query {
-    customQuery: String
+    getUsers: String
+  }
+
+  type Mutation {
+    newUser(input: UserInput): User
   }
 `;
 
