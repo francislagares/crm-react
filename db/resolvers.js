1cd5dc041eeae6918ce2b52bc12108d660226148
Francis Lagares <francis.lagares@gmail.com>
2020-09-10 17:06:18 +0200

    feat: create resolver and mutation to create new users

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,32 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 // Resolvers
 const resolvers = {
   Query: {},
+  Mutation: {
+    newUser: async (_, { input }) => {
+      // Check if user exists
+      const { email, password } = input;
+
+      const userExist = await User.findOne({ email });
+
+      if (userExist) {
+        throw new Error('User already exists');
+      }
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      input.password = await bcrypt.hash(password, salt);
+      // Save user on DB
+      try {
+        const user = new User(input);
+        user.save();
+        return user;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
