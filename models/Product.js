16ed7ff1bb731505c521e6f82959ccfa3a48c016
Francis Lagares <francis.lagares@gmail.com>
2020-09-10 20:32:14 +0200

    feat: create product model

---
+++ models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
